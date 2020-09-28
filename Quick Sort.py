b49e4fd945bb32b002e64eb63dde41d6b537dc81
Gideon4 <43590645+Gideon4@users.noreply.github.com>
2020-09-27 19:14:24 -0500

    Add files via upload

--- Quick Sort.py
+++ Quick Sort.py
@@ -1,26 +1,31 @@
 import random
 
-def sort(listo,cond,majorlist):
-    if len(listo)==1:
-        majorlist.append(listo[0])
-    elif listo==[]:
-        return
-    else:
-        newlist=[]
-        count=0
+def sort(listo):
+    if len(listo)>1:
         part=listo[0]
-        listo.pop(0)
-        for i in range(len(listo)):
-            if listo[i-count]>part:
-                newlist.append(listo[i-count])
-                listo.pop(i-count)
-                count+=1
-        sort(listo,False,majorlist)
-        sort([part],False,majorlist)
-        sort(newlist,False,majorlist)
-    if cond:
-        print(majorlist)
+        a,part,b=split(listo)
+        a=sort(a)
+        b=sort(b)
+        return a+part+b
+    else:
+        return listo
 
-sort([random.randint(0,1000000) for i in range(65536)],True,[])
-#print([random.randint(0,1000000) for i in range(65536)])
+def split(listo):
+    templist1=[]
+    templist2=[]
+    part=listo[0]
+    partlist=[part]
+    listo=listo[1:]
+    for item in listo:
+        if item>part:
+            templist1.append(item)
+        elif item<part:
+            templist2.append(item)
+        else:
+            partlist.append(item)
+    return templist2,partlist,templist1
 
+def generatelist(length,rangeo):
+    return [random.randint(0,rangeo) for i in range(length)]
+
+print(split(generatelist(4096,65536)))
